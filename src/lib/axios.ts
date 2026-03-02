4001473faea7b1fed7428b0969c67dffed62e0d2
kako351 <h3.birth@gmail.com>
2026-03-03 00:20:41 +0900

    interceptor追加

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,3 +5,16 @@ export const http = axios.create({
   baseURL: API_BASE_URL,
   timeout: 10000,
 });
+
+// エラーハンドリング
+http.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    switch (err.response?.status) {
+      case 404:
+        return Promise.reject(new Error('Not found.'));
+      default:
+        return Promise.reject(err);
+    }
+  },
+);
