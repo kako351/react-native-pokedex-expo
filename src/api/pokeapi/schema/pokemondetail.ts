1eb194917763c84bab94827321042ff3edd1dbdb
kako351 <h3.birth@gmail.com>
2026-03-05 22:37:56 +0900

    必要なパラメータ追加

--- src/api/pokeapi/schema/pokemondetail.ts
+++ src/api/pokeapi/schema/pokemondetail.ts
@@ -5,6 +5,7 @@ export const PokemonDetailSchema = z.object({
   name: z.string(),
   height: z.number(),
   weight: z.number(),
+  base_experience: z.number(),
   types: z.array(
     z.object({
       slot: z.number(),
@@ -14,15 +15,44 @@ export const PokemonDetailSchema = z.object({
       }),
     }),
   ),
+  stats: z.array(
+    z.object({
+      base_stat: z.number(),
+      effort: z.number(),
+      stat: z.object({
+        name: z.string(),
+        url: z.string(),
+      }),
+    }),
+  ),
+  abilities: z.array(
+    z.object({
+      is_hidden: z.boolean(),
+      slot: z.number(),
+      ability: z.object({
+        name: z.string(),
+        url: z.string(),
+      }),
+    }),
+  ),
+  moves: z.array(
+    z.object({
+      move: z.object({
+        name: z.string(),
+        url: z.string(),
+      }),
+    }),
+  ),
   sprites: z.object({
     front_default: z.string().nullable(),
     other: z
       .object({
-        'official-artwork': z.object({
-          front_default: z.string().nullable(),
-        }),
+        'official-artwork': z
+          .object({
+            front_default: z.string().nullable(),
+          })
+          .optional(),
       })
-      .partial()
       .optional(),
   }),
 });
